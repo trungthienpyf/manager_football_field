c0555b341125b9d0686a5a2cf20f1eb9c28b517f
Nguyen Trung Thien <269.tdd@gmail.com>
2022-05-14 09:35:57 +0700

    update

---
+++ database/migrations/2022_05_07_110521_create_category_peoples_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryPeoplesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_peoples', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_category');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_peoples');
+    }
+}
