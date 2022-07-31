3a23af6f8eb7636db81d67785478c5c955549263
Nguyen Trung Thien <269.tdd@gmail.com>
2022-07-31 16:18:02 +0700

    add Seeding test data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Area;
+use App\Models\Pitch;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
+         Area::factory(10)->create();
+         Pitch::factory(100)->create();
+            $this->call(PitchSeeder::class);
     }
 }
